5f465bb4951b3ed157f5b0e1af59ddd4d0592db3
Gary Burgess <argiebee@gmail.com>
2019-02-25 20:48:27 +0000

    Update with image

--- src/main/java/com/argiebee/dev/jxspringbooteg/HelloController.java
+++ src/main/java/com/argiebee/dev/jxspringbooteg/HelloController.java
@@ -14,9 +14,7 @@ public class HelloController {
 
 			new Image(1, "Treehouse of Horror V", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3842005760"),
 
-			new Image(2, "The Town", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3698134272"),
-
-			new Image(3, "The Last Traction Hero", "https://www.imdb.com/title/tt0096697/mediaviewer/rm1445594112"));
+			new Image(2, "snow 2010", "/images/snow2010.JPG"),
 
 		return images;
 
