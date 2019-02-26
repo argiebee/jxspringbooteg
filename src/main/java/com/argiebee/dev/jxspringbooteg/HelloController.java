9200bac04dd4d1e72986bb599a781120edc29eab
Gary Burgess <argiebee@gmail.com>
2019-02-26 08:07:11 +0000

    snow pic resource

--- src/main/java/com/argiebee/dev/jxspringbooteg/HelloController.java
+++ src/main/java/com/argiebee/dev/jxspringbooteg/HelloController.java
@@ -1,30 +1,19 @@
 package com.argiebee.dev.jxspringbooteg;
 
-import java.util.Arrays;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.eureka.image.entities.Image;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
-	@RequestMapping("/images")
-
-	public List<Image> getImages() {
-
-		List<Image> images = Arrays.asList(
-
-			new Image(1, "Treehouse of Horror V", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3842005760"),
-
-			new Image(2, "snow 2010", "/images/snow2010.JPG")
-		);
-
-		return images;
-
+    @RequestMapping(value = "/snow2010", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getImage() throws IOException {
+        var imgFile = new ClassPathResource("image/snow2010.JPG");
+        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(bytes);
     }
 
 }
